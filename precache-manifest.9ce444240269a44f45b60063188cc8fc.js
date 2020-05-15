362868d6a322f1cc449a660cd68f00e6ae90193a
pft\\Ruby_Feng <Ruby_Feng@PerfectCorp.com>
2020-05-15 11:44:01 +0800

    Updates

---
+++ precache-manifest.9ce444240269a44f45b60063188cc8fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9d318bb9f2c3c9a2b5fbb761313f65",
+    "url": "/faceAPIProject/index.html"
+  },
+  {
+    "revision": "557db0ab4bcbc2bf8114",
+    "url": "/faceAPIProject/static/css/main.b68dedff.chunk.css"
+  },
+  {
+    "revision": "ef462aee66738286c5ee",
+    "url": "/faceAPIProject/static/js/2.dd8126b0.chunk.js"
+  },
+  {
+    "revision": "d39744b49cd97a6a5d7904677be33b38",
+    "url": "/faceAPIProject/static/js/2.dd8126b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557db0ab4bcbc2bf8114",
+    "url": "/faceAPIProject/static/js/main.28e24984.chunk.js"
+  },
+  {
+    "revision": "4c190ce1b3c9b59ea29c",
+    "url": "/faceAPIProject/static/js/runtime-main.7511e5d1.js"
+  },
+  {
+    "revision": "1e6d9f0dcc1d26b4f840ed7d22a5c536",
+    "url": "/faceAPIProject/static/media/loading2.1e6d9f0d.gif"
+  },
+  {
+    "revision": "2677b93a54d626ec9eecb44672782363",
+    "url": "/faceAPIProject/static/media/upload.2677b93a.png"
+  }
+]);
